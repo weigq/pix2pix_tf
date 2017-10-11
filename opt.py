3d7dde4e65e1864d12cf1744348fdf65071a5959
weigq <weigq1234@gmail.com>
2017-10-11 09:52:46 -0400

    add opt.py

--- opt.py
+++ opt.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__mtime__ = '10/11/2017'
+import argparse
+
+__all__ = ['Options']
+
+
+class Options():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+
+    def _initial(self):
+        # =====================================================
+        #               General Options
+        # =====================================================
+        self.parser.add_argument("--input_dir",     type=str, default='facades/train', help="path to data path")
+        self.parser.add_argument("--output_dir",    type=str, default='facades/train', help="path to save result")
+        self.parser.add_argument("--checkpoint",    type=str, default=None)
+        self.parser.add_argument("--output_filetype", default="png", choices=["png", "jpeg"])
+        self.parser.add_argument("--seed",          type=int, default=None)
+        self.parser.add_argument("--lab_colorization", action="store_true", help="split input image into brightness (A) and color (B)")
+        # =====================================================
+        #               Model Options
+        # =====================================================
+        self.parser.add_argument("--mode",          type=str, default='train', choices=["train", "test", "export"])
+        self.parser.add_argument("--ngf",           type=int, default=64, help="# of G filters in 1st conv layer")
+        self.parser.add_argument("--ndf",           type=int, default=64, help="# of D filters in 1st conv layer")
+        self.parser.add_argument("--which_direction", type=str, default="AtoB", choices=["AtoB", "BtoA"])
+        # =====================================================
+        #               Running Options
+        # =====================================================
+        self.parser.add_argument("--batch_size",    type=int, default=6)
+        self.parser.add_argument("--scale_size",    type=int, default=286, help="image size before cropping to 256x256")
+        self.parser.add_argument("--aspect_ratio",  type=float, default=1.0, help="ratio of output images(w/h)")
+        self.parser.add_argument("--flip",          dest="flip", action="store_true", help="flip images horizontally")
+        self.parser.add_argument("--no_flip",       dest="flip", action="store_false", help="nor flip images horizontally")
+        self.parser.set_defaults(flip=True)
+        self.parser.add_argument("--lr",            type=float, default=0.0002)
+        self.parser.add_argument("--beta1",         type=float, default=0.5, help="momentum term of adam")
+        self.parser.add_argument("--l1_weight",     type=float, default=100.0, help="weight on L1 loss of G")
+        self.parser.add_argument("--gan_weight",    type=float, default=1.0, help="weight on GAN loss of G")
+        self.parser.add_argument("--max_steps",     type=int, help="# of training steps")
+        self.parser.add_argument("--max_epochs",    type=int, help="# of training epochs")
+        self.parser.add_argument("--summary_freq",  type=int, default=100, help="frequency of updating summary")
+        self.parser.add_argument("--progress_freq", type=int, default=50, help="frequency of display progress")
+        self.parser.add_argument("--trace_freq",    type=int, default=0, help="frequency of trace execution")
+        self.parser.add_argument("--display_freq",  type=int, default=0, help="frequency of saving current training images")
+        self.parser.add_argument("--save_freq",     type=int, default=5000, help="frequency of saving model")
+
+    def parse(self):
+        self.opt = self.parser.parse_args()
+        return self.opt
--- pix2pix.py
+++ pix2pix.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
-import argparse
 import os
 import json
 import glob
@@ -13,39 +12,9 @@ import collections
 import math
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_dir", help="path to folder containing images")
-parser.add_argument("--mode", required=True, choices=["train", "test", "export"])
-parser.add_argument("--output_dir", required=True, help="where to put output files")
-parser.add_argument("--seed", type=int)
-parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from or use for testing")
+from opt import Options
 
-parser.add_argument("--max_steps", type=int, help="number of training steps (0 to disable)")
-parser.add_argument("--max_epochs", type=int, help="number of training epochs")
-parser.add_argument("--summary_freq", type=int, default=100, help="update summaries every summary_freq steps")
-parser.add_argument("--progress_freq", type=int, default=50, help="display progress every progress_freq steps")
-parser.add_argument("--trace_freq", type=int, default=0, help="trace execution every trace_freq steps")
-parser.add_argument("--display_freq", type=int, default=0, help="write current training images every display_freq steps")
-parser.add_argument("--save_freq", type=int, default=5000, help="save model every save_freq steps, 0 to disable")
-
-parser.add_argument("--aspect_ratio", type=float, default=1.0, help="aspect ratio of output images (width/height)")
-parser.add_argument("--lab_colorization", action="store_true", help="split input image into brightness (A) and color (B)")
-parser.add_argument("--batch_size", type=int, default=1, help="number of images in batch")
-parser.add_argument("--which_direction", type=str, default="AtoB", choices=["AtoB", "BtoA"])
-parser.add_argument("--ngf", type=int, default=64, help="number of generator filters in first conv layer")
-parser.add_argument("--ndf", type=int, default=64, help="number of discriminator filters in first conv layer")
-parser.add_argument("--scale_size", type=int, default=286, help="scale images to this size before cropping to 256x256")
-parser.add_argument("--flip", dest="flip", action="store_true", help="flip images horizontally")
-parser.add_argument("--no_flip", dest="flip", action="store_false", help="don't flip images horizontally")
-parser.set_defaults(flip=True)
-parser.add_argument("--lr", type=float, default=0.0002, help="initial learning rate for adam")
-parser.add_argument("--beta1", type=float, default=0.5, help="momentum term of adam")
-parser.add_argument("--l1_weight", type=float, default=100.0, help="weight on L1 term for generator gradient")
-parser.add_argument("--gan_weight", type=float, default=1.0, help="weight on GAN term for generator gradient")
-
-# export options
-parser.add_argument("--output_filetype", default="png", choices=["png", "jpeg"])
-a = parser.parse_args()
+a = Options().parse()
 
 EPS = 1e-12
 CROP_SIZE = 256
@@ -540,7 +509,7 @@ def append_index(filesets, step=False):
 
 def main():
     if tf.__version__.split('.')[0] != "1":
-        raise Exception("Tensorflow version 1 required")
+        raise Exception("Tensorflow of version 1.0+ required")
 
     if a.seed is None:
         a.seed = random.randint(0, 2**31 - 1)
